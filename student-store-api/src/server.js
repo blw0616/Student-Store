30ac5d9e67de43cbe2c79c810309d942ae167bac
Bailey Williams <baileywilliams2026.com>
2024-06-24 13:26:20 -0700

    Starting project

--- student-store-api/src/server.js
+++ student-store-api/src/server.js
@@ -1,1 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
 
+app.get("/", (req, res) =>{
+    res.send("Hello girl!")
+})
+
+app. listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`)
+})
